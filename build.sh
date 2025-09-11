4bc1583ee16769c0fdabf0ac263a403a7eb70f3c
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-09-11 16:02:24 +0500

    Fix build.sh to use explicit production settings during build

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # Exit on error
 set -o errexit
 
+# Set Django environment
+export DJANGO_ENV=production
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --settings=storefront.production_settings
 
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --settings=storefront.production_settings
